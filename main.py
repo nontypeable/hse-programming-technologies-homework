fb39df0baa38f8725e71c7e065a69908f20bce07
Andrey Sadkov Dmitrievich <45257876+nontypeable@users.noreply.github.com>
2025-09-19 14:50:40 +0300

    BREAKING CHANGE: changing exponentiation to logarithm

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 number1 = int(input())
 number2 = int(input())
-print(f'Число {number1} в степени {number2}: {number1**number2}')
+print(f'Логарифм числа {number2} по основанию {number1}: {math.log(number2, number1)}')
